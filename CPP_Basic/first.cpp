257338f953e3324b09315c2d0b094843770577f3
GokceSoylu <gokcesoylu24@gmail.com>
2026-06-28 11:05:41 +0300

    basic

---
+++ CPP_Basic/first.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Gökçe";
+    return 0;
+}
---
+++ CPP_Basic/namespace_.cpp
@@ -0,0 +1,12 @@
+// namespace mantığı nedir
+// bizi sürekli std::cin vb std:: yazmaktan kurtarır
+// kulllandığımız namespace'in yani alanın std olduğunu batan belirtir
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a;
+    return 0;
+}
